2246513dcbffe2c7b5c8a47fa1b3f9e5ff2155fc
yash001dev <y.sanghavi001@gmail.com>
2022-04-22 18:13:43 +0530

    Basic Structure of nest Project created

---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  insertProduct(title: string, desc: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, desc, price);
+    this.products.push(newProduct);
+    return prodId;
+  }
+
+  getProducts() {
+    return [...this.products];
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const product = this.findProduct(productId)[0];
+    const index = this.findProduct(productId)[1];
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.desc = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id == id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
+    }
+    return [product, productIndex];
+  }
+}
